6228fcd6a28f9f490bfeebdd2e31b7512f17998f
Anna Medyukh <annamedyukh@gmail.com>
2017-10-03 18:31:39 -0400

    done for the day seeded database

--- models/index.js
+++ models/index.js
@@ -1,6 +1,66 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/tripPlanner', {
-    logging: false
+var db = new Sequelize('postgres://localhost:5432/tripPlanner');
+
+const Place = db.define('place', {
+    address: {
+        type: Sequelize.STRING
+    },
+    city: {
+        type: Sequelize.STRING
+    },
+    state: {
+        type: Sequelize.STRING
+    },
+    phone: {
+        type: Sequelize.STRING
+    },
+    location: {
+        type: Sequelize.ARRAY(Sequelize.DOUBLE)
+    },
 });
 
-module.exports= { db }
+const Hotel = db.define('hotel', {
+    name: {
+        type: Sequelize.STRING
+    },
+    num_stars: {
+        type: Sequelize.FLOAT
+    },
+    amenities: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+const Activity = db.define('activity', {
+    name: {
+        type: Sequelize.STRING
+    },
+    age_range: {
+        type: Sequelize.STRING
+    }
+});
+
+const Restaurant = db.define('restaurant', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    cuisine: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        validate: {
+            min: 1,
+            max: 5
+        }
+    },
+});
+
+Hotel.belongsTo(Place)
+Restaurant.belongsTo(Place)
+Activity.belongsTo(Place)
+
+module.exports= { db, Hotel, Activity, Restaurant, Place }
--- server/app.js
+++ server/app.js
@@ -16,11 +16,11 @@ app.set('view engine', 'html');
 
 
 
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
